b886d593bfc7cab1c74f3be73b449ac7958ab7e1
Levi Raichik <lraichik@gmail.com>
2019-06-04 13:52:54 -0400

    done

--- school.py
+++ school.py
@@ -1,1 +1,21 @@
-class School:
+class School(object):
+    def __init__(self, name=object, roster={}):
+        self.name = name
+        self.roster = roster
+    
+    def add_student(self, name=None, grade=None):
+        if grade in self.roster:
+            self.roster[grade].append(name)
+        else:
+            self.roster[grade] = [name]
+    
+    def grade(self, number=None):
+        return self.roster[number]
+    
+    def sort_roster(self):
+        for s in self.roster:
+            self.roster[s].sort()
+        return self.roster
+        
+
+
